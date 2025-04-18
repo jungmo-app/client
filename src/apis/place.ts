62614cb95e7847d36e0e4a83d4b8d2633299dee7
armd482 <armd482@naver.com>
2025-04-18 19:55:58 +0900

    fix: 지도 검색 좌표 값 변경

--- src/apis/place.ts
+++ src/apis/place.ts
@@ -10,13 +10,11 @@ import { apis } from '.';
 
 export const placeApis = {
   getSearchResult: async (keyword: string, center: google.maps.LatLng, radius: number) => {
-    const response = await fetch(
-      `/api/places/search?keyword=${keyword}&lat=${center.lat()}&lng=${center.lng}&radius=${radius}`,
-      {
-        method: 'GET',
-        next: { revalidate: 3600 },
-      }
-    );
+    const [lat, lng] = [center.lat(), center.lng()];
+    const response = await fetch(`/api/places/search?keyword=${keyword}&lat=${lat}&lng=${lng}&radius=${radius}`, {
+      method: 'GET',
+      next: { revalidate: 3600 },
+    });
 
     const result = (await response.json()) as ApiResponse<PlaceSearchDataType[]>;
 
