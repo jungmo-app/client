57347c6176fa8fd29c518342ab76430f542395a6
armd482 <armd482@naver.com>
2025-04-29 15:33:43 +0900

    fix: 스켈레톤, 이미 fetch된 일정 수정

--- src/app/(main)/appointmentList.tsx
+++ src/app/(main)/appointmentList.tsx
@@ -9,8 +9,15 @@ import { useAppointmentList } from '@/hooks/useQuery/useAppointmentList';
 import AppointmentCard from './appointmentCard';
 
 export default function AppointmentList() {
-  const { data: appointments, isPending } = useAppointmentList();
-  const [cardLoad, setCardLoad] = useState<Set<number>>(new Set());
+  const { data: appointments, isPending, isFetched } = useAppointmentList();
+  const [cardLoad, setCardLoad] = useState<Set<number>>(() => {
+    if (isFetched) {
+      const set = new Set<number>();
+      appointments?.forEach(appointment => set.add(appointment.id));
+      return set;
+    }
+    return new Set();
+  });
 
   const handleCardReady = useCallback((id: number) => {
     setCardLoad(prev => new Set(prev).add(id));
