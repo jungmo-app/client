d68a59c2487198e4d52c1823f667a550201d4ba3
armd482 <armd482@naver.com>
2025-05-02 16:19:45 +0900

    style: mobile-layout 변경 및 sheet 수정(오른쪽, 왼쪽 모바일 layout에 맞춰 변경)

--- src/components/layouts/MobileLayout.tsx
+++ src/components/layouts/MobileLayout.tsx
@@ -7,8 +7,11 @@ type MobileLayoutProps = StrictPropsWithChildren<{
 
 const MobileLayout = ({ children, className }: MobileLayoutProps) => {
   return (
-    <div className="relative mx-auto flex min-h-screen max-w-mobile flex-col bg-background">
-      <div className={cn('h-full w-full', className)}>{children}</div>
+    <div
+      id="mobile-root"
+      className="relative mx-auto flex h-screen max-w-mobile flex-col overflow-hidden bg-background"
+    >
+      <div className={cn('h-full w-full overflow-auto', className)}>{children}</div>
     </div>
   );
 };
