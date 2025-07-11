210daac9de011b5bc5653b0761679b1358c1c33c
armd482 <armd482@naver.com>
2025-07-11 11:15:01 +0900

    chore: 불필요한 코드 제거

--- src/constants/routes.ts
+++
@@ -1,5 +0,0 @@
-export const BASE_URL = '/';
-
-export const PUBLIC_ROUTES: string[] = [];
-
-export const PRIVATE_ROUTES: string[] = [];
--- src/libs/serverAction.ts
+++ src/libs/serverAction.ts
@@ -1,24 +1,11 @@
 'use server';
 
-import { revalidatePath, revalidateTag } from 'next/cache';
 import { ResponseCookie } from 'next/dist/compiled/@edge-runtime/cookies';
 import { cookies } from 'next/headers';
-import { redirect } from 'next/navigation';
 
 export const getCookie = async (name: string) => {
   const cookie = cookies().get(name)?.value;
   return cookie;
-};
-
-export const getCookieList = async (names: string[]) => {
-  const cookieStore = cookies();
-  return names.reduce(
-    (acc, name) => {
-      acc[name] = cookieStore.get(name)?.value;
-      return acc;
-    },
-    {} as Record<string, string | undefined>
-  );
 };
 
 export const setCookie = async (name: string, value: string, option?: Partial<ResponseCookie>) => {
@@ -29,30 +16,4 @@ export const setCookie = async (name: string, value: string, option?: Partial<Re
     path: '/',
     ...option,
   });
-};
-
-export const redirectPath = (url: string) => {
-  redirect(url);
-};
-
-export const revalidatePage = (url: string) => {
-  revalidatePath(url);
-};
-
-export const revalidateData = (tag: string) => {
-  revalidateTag(tag);
-};
-
-export const redirectLogin = () => {
-  const accessToken = cookies().get('accessToken')?.value;
-  if (!accessToken) {
-    redirect('/login');
-  }
-};
-
-export const redirectMain = () => {
-  const accessToken = cookies().get('accessToken')?.value;
-  if (accessToken) {
-    redirect('/');
-  }
 };
